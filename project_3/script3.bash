cac9f77ac986875f34a926bf701285d3b17914d8
Mohammad <sajadrahmanian1616@gmail.com>
2022-12-11 18:01:07 +0330

    add script3

---
+++ project_3/script3.bash
@@ -0,0 +1,19 @@
+sum=0
+while true
+do
+    echo "please enter a number"
+    read number 
+    if ! [[ $number =~ ^[0-9]+$ ]];
+    then echo "error: variable 1 not a number"; exit 1
+    fi
+    strlen=${#number}
+    reverse_str=""
+    for (( i=$strlen-1; i>=0; i-- ))
+    do
+        reverse_str=$reverse_str${number:$i:1}
+    done
+    echo "Original String : $number"
+    echo "Reversed String : $reverse_str"
+    sum=$(($sum + $number))
+    echo "Sum : $sum"
+done
