843cc8cb85932c175fd0426b38fa6e2e514b64bb
sina shariati <sina.shariati@yahoo.com>
2022-12-11 17:49:52 +0330

    Finish script_6 of project 3

---
+++ project_3/script6.bash
@@ -0,0 +1,25 @@
+# !/bin/bash
+operand1=12.6;
+operand2=2;
+operator="/"
+result=0;
+
+case $operator in
+    "+")
+    result=$(echo $operand1 + $operand2 | bc);
+    ;;
+
+    "-")
+    result=$(echo $operand1 - $operand2 | bc);
+    ;;
+
+    "*")
+    result=$(echo $operand1 * $operand2 | bc);
+    ;;
+
+    "/")
+    result=$(echo scale=4\;$operand1 / $operand2 | bc);
+    ;;
+esac
+
+echo $result
