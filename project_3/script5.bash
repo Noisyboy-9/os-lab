bba2e58ae0826f6129773ae717d2948a73788756
sina shariati <sina.shariati@yahoo.com>
2022-12-11 17:41:24 +0330

    Finish script_5 of project 3

---
+++ project_3/script5.bash
@@ -0,0 +1,62 @@
+function print_numbers(){
+	for ((i=1;i<=5;i++))
+	do
+		for j in $(seq 1 $i)
+		do
+			echo -n "$i "
+		done
+		echo
+	done
+}
+
+function print_diamond(){
+	for ((i=1;i<=6;i++))
+	do
+		for ((j=5;j>=$i;j--))
+		do
+			echo -n " "
+		done
+		for j in $(seq 1 $i)
+		do
+			echo -n "* "
+		done
+		echo
+	done
+	for ((i=1;i<=6;i++))
+	do
+		for ((j=1;j<$i;j++))
+		do
+			echo -n " "
+		done
+		for ((j=$i;j<=6;j++))
+		do
+			echo -n "* "
+		done
+		echo
+	done
+	
+}
+
+function print_steps(){
+	for ((i=1;i<=5;i++))
+	do
+		
+		for ((j=1;j<$i;j++))
+		do
+			echo -n "| "
+		done
+		echo "|_"
+	done
+}
+
+case $1 in
+	"1")
+	print_numbers	
+		;;
+	"2")
+	print_diamond	
+		;;
+	"3")
+	    print_steps	
+		;;	
+esac
